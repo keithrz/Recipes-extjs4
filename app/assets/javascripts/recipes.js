bf0110932f645da3d9979e6c5c9929b5e64690a0
Keith Zimmerman <keithrz@gmail.com>
2011-11-22 12:29:34 -0600

    delete works now

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
 	$("#recipe-grid").jqGrid({ 
+		//
 		// specific to recipes
-	    url:'recipes.json',
-		
+	    //
+		url:'recipes.json',
+		colNames:[
+			'Actions',
+			'Title',
+			'Content'
+		], 
+		colModel :[ 
+		    {name:'actions', width:60, fixed:true, sortable:false, resize:false, formatter: 'actions'},
+			{name:'title', index:'title',editable:true}, 
+			{name:'content', index:'content',editable:true,edittype:'textarea', editoptions:{rows:5, cols:20}}, 
+		],
+		pager: '#pager',
+    	rowNum: 10,
+    	rowList: [10,25,50,100],
+    	sortname: 'title',
+    	sortorder: 'asc',
+    	caption: 'Recipe List',
 		//TODO move 'recipe' string to recipe-specific code
 		//     move rest of serializeRowData to boilerplate
 		serializeRowData: function(postData){
@@ -20,22 +37,7 @@ $(document).ready(function(){
 			rootData['recipe'] = recipeData;
 			return rootData;
 		},
-		colNames:[
-			'Actions',
-			'Title',
-			'Content'
-		], 
-		colModel :[ 
-		    {name:'actions', width:60, fixed:true, sortable:false, resize:false, formatter: 'actions'},
-			{name:'title', index:'title',editable:true}, 
-			{name:'content', index:'content',editable:true,edittype:'textarea', editoptions:{rows:5, cols:20}}, 
-		],
-		pager: '#pager',
-    	rowNum: 10,
-    	rowList: [10,25,50,100],
-    	sortname: 'title',
-    	sortorder: 'asc',
-    	caption: 'Recipe List',
+
 		// TODO move 'recipes' string to recipe-specific code
 		//      move rest of onSelectRow to boilerplate section
 		onSelectRow: function(id) {
@@ -45,26 +47,44 @@ $(document).ready(function(){
 			}
 		},
 
+		//
 		//boilerplate for RoR, ajax
-		//TODO if creating 2nd grid, move this to a common Obj
+		//
+		//TODO if creating 2nd grid, move all boilerplate to a common Obj
 		datatype: 'json',
 		jsonReader: {
 			repeatitems: false
 		},
+		
+		delOptions: {
+			mtype: 'DELETE',
+			ajaxDelOptions: {
+				//contentType: 'application/json; charset=utf-8',
+
+				// For successful PUTs and DELETEs, RoR returns a single whitespace
+				// jQuery.ajaxConvert() does not like parsing the whitespace
+				// this is a workaround
+				dataFilter: function(responseData, dataType) {
+					if(responseData.trim() == "") {
+						return "";
+					} else {
+						return responseData;
+					}
+				}
+			}				
+		},
+		//editOptions: {mtype: 'PUT'},
+		
 		ajaxRowOptions: { 
 			//contentType: 'application/json; charset=utf-8',
-
-			// For successful PUTs, RoR returns a single whitespace
-			// jQuery.ajaxConvert() does not like parsing the whitespace
-			// this is a workaround
-			dataFilter: function(responseData, dataType){
+			type: "PUT",
+			dataFilter: function(responseData, dataType) {
 				if(responseData.trim() == "") {
 					return "";
 				} else {
 					return responseData;
 				}
-			},
-			type: "PUT" 
+			}
 		},
     	viewrecords: true
   	});
